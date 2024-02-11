3c8a3561de08c231d9edbcf6fbb4b7b4f2ba6d8a
thewhiteant <trafi227@gmail.com>
2024-02-11 14:44:14 +0600

    Whiteant

--- reservation.php
+++ reservation.php
@@ -1,33 +1,41 @@
 <?php
 
+$host = "localhost";
+$usr = "root";
+$pass = "";
+$database = "mydb";
 
-// $host = "localhost";
-// $usr = "root";
-// $pass = "";
-// $database = "mydb";
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Create connection
+    $ser = new mysqli($host, $usr, $pass, $database);
 
-// $ser = new mysqli($host, $usr, $pass, $database);
+    // Check connection
+    if ($ser->connect_error) {
+        die("Connection failed: " . $ser->connect_error);
+    }
 
+    // Prepare and bind SQL statement
+    $sql = "INSERT INTO RESERVE_Table (name, number, person, date, time, msg) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $ser->prepare($sql);
+    $stmt->bind_param("ssisss", $name, $number, $person, $date, $time, $message);
 
-$name=$_POST["name"];
-$number=$_POST["phone"];
-$person=$_POST["person"];
-$date=$_POST["reservation-date"];
-$time=$_POST["time-outline"];
-$message=$_POST["message"];
+    // Set parameters and execute
+    $name = $_POST["name"];
+    $number = $_POST["phone"];
+    $person = $_POST["person"];
+    $date = $_POST["reservation-date"];
+    $time = $_POST["time-outline"];
+    $message = $_POST["message"];
 
-echo "<script> $name </script>"
+    if ($stmt->execute()) {
+        echo "<script>alert('Reservation Complete')</script>";
+        header("Location: http://localhost/");
+    } else {
+        echo "Error: " . $sql . "<br>" . $ser->error;
+    }
 
-// $sql = "INSER INTO RESERVE_Table (name,number,person,date,time,msg) values ($name,$number,$person,$date,$time,$message);";
-
-// if ($ser->query($sql)) {
-//     echo "<script> alert(' Reservation Complete') </script>";
-//     header("Location: http://localhost/");
-// } else {
-//     echo "Error: " . $sql . "<br>" . $ser->error;
-// }
-
-// $ser->close();
-
-
+    $stmt->close();
+    $ser->close();
+}
 ?>
