d199c56313b44f55107829ac2b52c2565f1484fe
ashrafuldalif <ashrafulalif26@gmail.com>
2025-03-08 19:05:25 +0600

    ei nen rafsan the boro vai

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,48 @@
+// introduction to spl
+// basic structure of c
+// variables (int float char double long)
+// printf 
+// scanf
+// operators (+ - * / % ++ -- > < >= <= == != && || !)
+// if else if else switch case
+// loops (for while do while){increment i++ decrement i--}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+int main()
+{ 
+    int a = 10
+
+    printf("Hello World\n");
+    return 0;
+}
